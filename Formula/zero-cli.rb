30c25efb89d598eaab243761c254af6256712ba9
Misha-Otto <michael.zacharov@ottofeller.com>
2024-04-29 21:40:58 +0200

    update formula

--- Formula/zero-cli.rb
+++ Formula/zero-cli.rb
@@ -1,9 +1,9 @@
 class ZeroCli < Formula
   desc "A command line tool for Zero Secrets Manager"
   homepage "https://github.com/zerosecrets/cli"
-  url "https://github.com/zerosecrets/cli/releases/download/v2.1.0/zero-cli-2.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "0c665528c9c57d4e848df75b46b58753b64d4604b82798e244f5fe807ee2118b"
-  version "2.1.0"
+  url "https://github.com/zerosecrets/cli/releases/download/v2.2.0/zero-cli-2.2.0-x86_64-apple-darwin.tar.gz"
+  sha256 "219ed3a6c9cd826122dd742d8e1d9c3e6d2dd3458075e317f4a541310e423e38"
+  version "2.2.0"
 
   def install
     bin.install "zero-cli"
